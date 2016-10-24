cc950a695f5f4d5735352181238f518134c0479a
Peter Beshai <peter.beshai@gmail.com>
2016-10-24 18:07:02 -0400

    Refactor AddISPsFn to use BaseIPMatchingFn

--- dataflow/src/main/java/mlab/bocoup/dofn/AddISPsFn.java
+++ dataflow/src/main/java/mlab/bocoup/dofn/AddISPsFn.java
@@ -1,15 +1,8 @@
 package mlab.bocoup.dofn;
 
-import java.util.Map;
 import java.util.NavigableMap;
 
-import javax.xml.bind.DatatypeConverter;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.google.api.services.bigquery.model.TableRow;
-import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.values.PCollectionView;
 
 /**
@@ -19,161 +12,44 @@ import com.google.cloud.dataflow.sdk.values.PCollectionView;
  * @author pbeshai
  *
  */
-public class AddISPsFn extends DoFn<TableRow, TableRow> {
-	private static final Logger LOG = LoggerFactory.getLogger(AddISPsFn.class);
-
-	// values for the IP family field
-	private static final int IP_FAMILY_IPV4 = 2;
-	private static final int IP_FAMILY_IPV6 = 10;
-
-	/** Side input view NavigableMap from min IP to AS data */
-	private PCollectionView<NavigableMap<String, TableRow>> asnView;
-
-	/**
-	 * Key in the data row to look up the IP family (IPV4 or IPV6 as int)
-	 */
-	private String dataIPFamilyKey;
-
-	/**
-	 * The base64 encoded IP key
-	 */
-	private String dataIPBase64Key;
-
+public class AddISPsFn extends BaseIPMatchingFn {
+	
 	/** Key in the data row to save the AS name to */
-	private String dataASNNameKey;
-
+	protected String dataASNNameKey;
 	/** Key in the data row to save the AS number to (optional) */
-	private String dataASNNumberKey;
-
-	/** Key in the AS row to look up the min IP hex from */
-	private String asnMinIPv4Key;
-
-	/** Key in the AS row to look up the max IP hex from */
-	private String asnMaxIPv4Key;
-
-	/** Key in the AS row to look up the min IPv6 hex from */
-	private String asnMinIPv6Key;
-
-	/** Key in the AS row to look up the max IPv6 hex from */
-	private String asnMaxIPv6Key;
-
+	protected String dataASNNumberKey;
+	
 	/** Key in the AS row to look up the AS name from */
-	private String asnNameKey;
-
+	protected String asnNameKey;
 	/** Key in the AS row to look up the AS number from (optional) */
-	private String asnNumberKey;
+	protected String asnNumberKey;
 
 	/**
 	 * Main constructor for the DoFn
 	 *
 	 * @param asnView
 	 * @param dataIPFamilyKey
-	 * @param dataASNNameKey
-	 * @param dataASNNumberKey
+	 * @param dataIPBase64Key
 	 * @param asnMinIPv4Key
 	 * @param asnMaxIPv4Key
+	 * @param asnMinIPv6Key
+	 * @param asnMaxIPv6Key
+	 * @param dataASNNameKey
+	 * @param dataASNNumberKey
 	 * @param asnNameKey
 	 * @param asnNumberKey
 	 */
-	public AddISPsFn(PCollectionView<NavigableMap<String, TableRow>> asnView, String dataIPFamilyKey, String dataIPBase64Key,
-			String dataASNNameKey, String dataASNNumberKey, String asnMinIPv4Key, String asnMaxIPv4Key,
-			String asnMinIPv6Key, String asnMaxIPv6Key, String asnNameKey, String asnNumberKey) {
-		super();
-		this.asnView = asnView;
-		this.dataIPFamilyKey = dataIPFamilyKey;
-		this.dataIPBase64Key = dataIPBase64Key;
+	public AddISPsFn(PCollectionView<NavigableMap<String, TableRow>> asnView, String dataIPFamilyKey,
+			String dataIPBase64Key, String asnMinIPv4Key, String asnMaxIPv4Key, String asnMinIPv6Key,
+			String asnMaxIPv6Key, String dataASNNameKey, String dataASNNumberKey, String asnNameKey,
+			String asnNumberKey) {
+		super(asnView, dataIPFamilyKey, dataIPBase64Key, asnMinIPv4Key, asnMaxIPv4Key, asnMinIPv6Key, asnMaxIPv6Key);
 		this.dataASNNameKey = dataASNNameKey;
 		this.dataASNNumberKey = dataASNNumberKey;
-		this.asnMinIPv4Key = asnMinIPv4Key;
-		this.asnMaxIPv4Key = asnMaxIPv4Key;
-		this.asnMinIPv6Key = asnMinIPv6Key;
-		this.asnMaxIPv6Key = asnMaxIPv6Key;
 		this.asnNameKey = asnNameKey;
 		this.asnNumberKey = asnNumberKey;
 	}
-
-	/**
-	 * Converts a packed IP in base64 to base16. Needed to maintain sort order
-	 *
-	 * @param base64Ip
-	 *            The packed base64 string (result of PARSE_PACKED_IP in SQL)
-	 * @return the IP as a base16 string
-	 */
-	public static String hexFromBase64(String base64Ip) {
-		byte[] decoded = DatatypeConverter.parseBase64Binary(base64Ip);
-		String hex = DatatypeConverter.printHexBinary(decoded);
-		
-		// ensure that the hex is the full 32 characters in length
-		int hexLength = hex.length();
-		if (hexLength < 32) {
-			String zeros = "00000000000000000000000000000000";
-			hex = zeros.substring(0,  32 - hexLength) + hex;
-		}
-		
-		return hex;
-	}
-
-	/**
-	 * The main function for matching AS based on data row IP and the IP range
-	 * the AS matches.
-	 *
-	 * This adds the AS to the data row in the columns specified by
-	 * dataASNNameKey and dataASNNumberKey.
-	 */
-	@Override
-	public void processElement(DoFn<TableRow, TableRow>.ProcessContext c) throws Exception {
-		// get the data row to modify
-		TableRow dataRow = c.element().clone();
-
-		// get the IP of the row
-		String ipBase64 = (String) dataRow.get(this.dataIPBase64Key);
-
-		// no IP configured, return the row as it is
-		if (ipBase64 == null) {
-			c.output(dataRow);
-			return;
-		}
-
-		String asnMinKey, asnMaxKey;
-		// check if it is IPv6
-		if (Integer.valueOf((String) dataRow.get(this.dataIPFamilyKey)) == IP_FAMILY_IPV6) {
-			// it is IPv6
-			asnMinKey = this.asnMinIPv6Key;
-			asnMaxKey = this.asnMaxIPv6Key;
-		} else {
-			// it is IPv4
-			asnMinKey = this.asnMinIPv4Key;
-			asnMaxKey = this.asnMaxIPv4Key;
-		}
-
-
-		// read in the navigable map from side input
-		NavigableMap<String, TableRow> asnMap = c.sideInput(this.asnView);
-
-		// we do have an IP, so look up the AS
-		String ipHex = hexFromBase64(ipBase64);
-
-		// find in map looking to match the min IP in the range
-		Map.Entry<String, TableRow> asnEntry = asnMap.floorEntry(ipHex);
-		if (asnEntry != null) {
-			TableRow asn = asnEntry.getValue();
-			// if an AS matches this IP, double check the IP is in range and
-			// set value
-			if (asn != null) {
-				String asnMinIp = (String) asn.get(asnMinKey);
-				String asnMaxIp = (String) asn.get(asnMaxKey);
-
-				// if it matches the IP range, add the AS to the data row
-				if (asnMinIp.compareTo(ipHex) <= 0 && asnMaxIp.compareTo(ipHex) >= 0) {
-					addASNInformation(dataRow, asn);
-				} 
-			}
-		}
-
-		// output the row with the new data added to it.
-		c.output(dataRow);
-	}
-
+	
 	/**
 	 * Helper function to add AS information to the data row if a match is
 	 * found.
@@ -183,7 +59,8 @@ public class AddISPsFn extends DoFn<TableRow, TableRow> {
 	 * @param asn
 	 *            The AS row
 	 */
-	private void addASNInformation(TableRow dataRow, TableRow asn) {
+	@Override
+	protected void addInfoToRow(TableRow dataRow, TableRow asn) {
 		// add in the AS name
 		dataRow.set(this.dataASNNameKey, asn.get(this.asnNameKey));
 		
@@ -192,5 +69,4 @@ public class AddISPsFn extends DoFn<TableRow, TableRow> {
 			dataRow.set(this.dataASNNumberKey, asn.get(this.asnNumberKey));
 		}
 	}
-
 }

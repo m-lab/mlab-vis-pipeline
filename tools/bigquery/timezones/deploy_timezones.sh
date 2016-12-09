a1e01aec2fcc2bcd393955dba9979ba5a7023355
Chris Ritzo <critzo@opentechinstitute.org>
2016-12-09 16:00:30 -0500

    updated path

--- tools/bigquery/timezones/deploy_timezones.sh
+++ tools/bigquery/timezones/deploy_timezones.sh
@@ -3,7 +3,7 @@
 echo "Creating timezone tables"
 
 echo "Processing timezones csvs CSV"
-python ./tools/timezones/process_timezones.py
+python ./tools/bigquery/timezones/process_timezones.py
 
 echo "Adding bocoup.localtime_timezones to BigQuery"
 bq load --allow_quoted_newlines --skip_leading_rows=1 --source_format=CSV \

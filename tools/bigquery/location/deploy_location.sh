1e2000a5a7c8e086f16604469b14cfc1008954fb
fernandalavalle <lavalle@opentechinstitute.org>
2016-12-12 15:54:41 -0500

    Updated table names in batch_run json files

--- tools/bigquery/location/deploy_location.sh
+++ tools/bigquery/location/deploy_location.sh
@@ -2,14 +2,14 @@
 
 echo "Creating location tables"
 
-echo "Add bocoup.location_country_codes"
+echo "Add data_viz.location_country_codes"
 bq load --allow_quoted_newlines --skip_leading_rows=1 --source_format=CSV \
   data_viz.location_country_codes  \
   ./dataflow/data/bigquery/location/iso3166.csv \
   ./dataflow/data/bigquery/location/schemas/iso3166.json
 
 
-echo "Add bocoup.location_region_codes"
+echo "Add dataviz.location_region_codes"
 bq load --allow_quoted_newlines --skip_leading_rows=1 --source_format=CSV \
   data_viz.location_region_codes  \
   ./dataflow/data/bigquery/location/region_codes.csv \

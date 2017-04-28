2ff413e2c6dd81c86bb82301110503326c29cf8c
fernandalavalle <lavalle@opentechinstitute.org>
2017-04-28 15:37:35 -0400

    Adding almost working staging versions of these scripts to be udpated with a production credentials file

--- run.sh
+++ run.sh
@@ -2,16 +2,17 @@
 
 # Run all parts of the pipeline with a provided end date.
 # options:
-#    -s <YYYY-MM-DD>: start date to run pipeline to.
+#    -s <YYYY-MM-DD>: start date to run pipeline from.
 #    -e <YYYY-MM-DD>: end date to run pipeline to.
-#    -t : to do a test run (doesn't start dataflow)
+#    -t: to do a test run (doesn't start dataflow)
 
-usage() { echo "Usage: $0 -e <YYYY-MM-DD> [-t]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 -s <YYYY-MM-DD> -e <YYYY-MM-DD> [-t]" 1>&2; exit 1; }
+STARTDATE=""
 ENDDATE=""
 TEST=0
 
-while getopts ":set:" opt; do
-  case $opt in
+while getopts "s:e:t" opt; do
+   case $opt in
     s)
       STARTDATE=${OPTARG}
       ;;
@@ -33,9 +34,10 @@ while getopts ":set:" opt; do
   esac
 done
 
-if [ -z "${ENDDATE}" ]; then
+if [ -z "${ENDDATE}" ] || [ -z "${STARTDATE}" ]; then
   usage
 fi
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATAFLOW_DIR="${DIR}/dataflow"
@@ -47,7 +49,7 @@ if [ ! -f $JAR_FILE ]; then
   exit 1;
 fi
 
-
+echo "Start date: ${STARTDATE}"
 echo "End date: ${ENDDATE}"
 echo "STARTING PIPELINE"
 
@@ -57,13 +59,13 @@ cd ${DATAFLOW_DIR}
 echo "Running historic pipeline for DAY"
 java -jar ${JAR_FILE} \
   --runner=com.google.cloud.dataflow.sdk.runners.DataflowPipelineRunner \
-  --timePeriod="day" --project=mlab-oti --stagingLocation="gs://bocoup" \
+  --timePeriod="day" --project=mlab-oti --stagingLocation="gs://mlab-data-viz" \
   --skipNDTRead=0 --startDate=${STARTDATE} --endDate=${ENDDATE} --test=${TEST} &
 
 echo "Running historic pipeline for HOUR"
 java -jar ${JAR_FILE} \
   --runner=com.google.cloud.dataflow.sdk.runners.DataflowPipelineRunner \
-  --timePeriod="hour" --project=mlab-oti --stagingLocation="gs://bocoup" \
+  --timePeriod="hour" --project=mlab-oti --stagingLocation="gs://mlab-data-viz" \
   --skipNDTRead=0 --startDate=${STARTDATE} --endDate=${ENDDATE} --test=${TEST} &
 
 # wait for these to complete.
@@ -72,5 +74,5 @@ wait
 echo "Running Bigtable Transfer Pipeline"
 java -cp ${JAR_FILE} mlab.bocoup.BigtableTransferPipeline \
   --runner=com.google.cloud.dataflow.sdk.runners.DataflowPipelineRunner \
-  --project=mlab-oti --stagingLocation="gs://bocoup" --test=${TEST} \
+  --project=mlab-oti --stagingLocation="gs://mlab-data-viz" --test=${TEST} \
   --diskSizeGb=15

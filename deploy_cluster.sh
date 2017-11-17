39333f3b376899246f3c12260d2442f132945f54
Irene Ros <imirene@gmail.com>
2017-11-17 10:45:16 -0500

    moving pod configs to deployment

--- deploy_cluster.sh
+++ deploy_cluster.sh
@@ -88,8 +88,6 @@ fi
 # Update container
 kubectl apply -f deploy-build/k8s/configmap.yaml
 kubectl apply -f deploy-build/k8s/deployment.yaml
-kubectl apply -f deploy-build/k8s/bqpipeline.yaml --force
-kubectl apply -f deploy-build/k8s/btpipeline.yaml --force
 
 echo "Your service is being created. You might need to wait a few minutes."
 echo "Run kubectl proxy to see the status."

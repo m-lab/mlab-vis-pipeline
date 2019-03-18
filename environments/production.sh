f1a9a75aa65085d67fe994ff0d25db2cd6a89ec1
Balázs Bartl <bartlbalazs@gmail.com>
2019-03-18 16:23:25 +0100

    Update environment settings with staging location

--- environments/production.sh
+++ environments/production.sh
@@ -2,11 +2,12 @@
 
 ###
 # Production environment variable setup
+# Important: STAGING_LOCATION cannot be set to a root directory of a bucket
 ###
 
 PROJECT=mlab-oti
 BIGTABLE_INSTANCE=mlab-data-viz-prod-2018
 API_MODE=production
 BIGTABLE_POOL_SIZE=40
-STAGING_LOCATION=gs://viz-pipeline-production-2018
+STAGING_LOCATION=gs://viz-pipeline-production-2018/staging
 K8_CLUSTER=viz-pipeline

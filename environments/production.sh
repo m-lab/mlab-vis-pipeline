c13a6ff7baeac29b85fac511407f2d2aae3faafd
Chris Ritzo <critzo@opentechinstitute.org>
2019-03-20 21:58:27 -0400

    changed pipeline cluster and storage location, updated gcloud indexes command

--- environments/production.sh
+++ environments/production.sh
@@ -6,8 +6,8 @@
 ###
 
 PROJECT=mlab-oti
-BIGTABLE_INSTANCE=mlab-data-viz-prod-2018
+BIGTABLE_INSTANCE=viz-pipeline
 API_MODE=production
 BIGTABLE_POOL_SIZE=40
-STAGING_LOCATION=gs://viz-pipeline-production-2018/staging
+STAGING_LOCATION=gs://viz-pipeline-production/production
 K8_CLUSTER=viz-pipeline
